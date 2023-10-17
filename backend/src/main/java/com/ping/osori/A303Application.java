d18f18176884b0e505e967f90ecec707736153f0
mydowson <mydowson@gmail.com>
2023-10-17 16:11:11 +0900

    Initial commit

---
+++ backend/src/main/java/com/ping/osori/A303Application.java
@@ -0,0 +1,13 @@
+package com.ping.osori;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A303Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(A303Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ping/osori/A303ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ping.osori;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class A303ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
