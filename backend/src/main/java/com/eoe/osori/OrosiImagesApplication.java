74b4295455714f9e251e63a68a67bc19af88dd77
김현수 <gustn2615@naver.com>
2023-10-26 17:06:19 +0900

    :tada: 프로젝트 이미지 서비스 생성(develop/images) 프로젝트 이미지 서비스 branch 생성

--- backend/src/main/java/com/eoe/osori/OrosiImagesApplication.java
+++ backend/src/main/java/com/eoe/osori/OrosiImagesApplication.java
@@ -1,15 +1,13 @@
 package com.eoe.osori;
 
-import com.eoe.osori.global.advice.exception.GlobalControllerAdvice;
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class A303Application {
+public class OrosiImagesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(A303Application.class, args);
+		SpringApplication.run(OrosiImagesApplication.class, args);
 	}
 
 }
