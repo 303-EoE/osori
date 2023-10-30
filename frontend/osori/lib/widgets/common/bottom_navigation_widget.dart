5a0ed64ddbb38a06e9efa135c7bf80ebc8fb46e9
sweetandsourkiss <mooul0529@gmail.com>
2023-10-30 11:09:40 +0900

    :art: 네비게이션 바 구조 변경
    - NavigationDestination 활용
    - 선택/비선택 상태별 아이콘 구분
    - selectedIndex 활용

--- frontend/osori/lib/widgets/common/bottom_navigation_widget.dart
+++ frontend/osori/lib/widgets/common/bottom_navigation_widget.dart
@@ -1,73 +1,81 @@
 import 'package:flutter/material.dart';
 
-class BottomNavigation extends StatelessWidget {
+class BottomNavigation extends StatefulWidget {
   const BottomNavigation({
     super.key,
   });
 
   @override
+  State<BottomNavigation> createState() => _BottomNavigationState();
+}
+
+class _BottomNavigationState extends State<BottomNavigation> {
+  late int selectedIndex;
+
+  @override
   Widget build(BuildContext context) {
+    switch (ModalRoute.of(context)?.settings.name) {
+      case '/':
+        selectedIndex = 0;
+        break;
+      case '/map':
+        selectedIndex = 1;
+        break;
+      case '/chat':
+        selectedIndex = 2;
+        break;
+      case '/profile':
+        selectedIndex = 3;
+        break;
+      default:
+    }
     return NavigationBar(
-      destinations: [
-        GestureDetector(
-          onTapUp: (details) {
-            Navigator.of(context).pushNamedAndRemoveUntil(
-              '/',
-              (route) => route.isFirst,
-            );
-          },
-          child: Icon(
-            Icons.feed_outlined,
-            size: 32,
-            color: ModalRoute.of(context)?.settings.name == "/"
-                ? Colors.grey
-                : Colors.black,
-          ),
+      labelBehavior: NavigationDestinationLabelBehavior.alwaysHide,
+      surfaceTintColor: const Color(0xFFf9f8f8),
+      indicatorColor: const Color(0xFFe8e8e8),
+      selectedIndex: selectedIndex,
+      onDestinationSelected: (value) {
+        switch (value) {
+          case 0:
+            Navigator.of(context)
+                .pushNamedAndRemoveUntil('/', (route) => route.isFirst);
+            break;
+          case 1:
+            Navigator.of(context)
+                .pushNamedAndRemoveUntil('/map', (route) => route.isFirst);
+            break;
+          case 2:
+            Navigator.of(context)
+                .pushNamedAndRemoveUntil('/chat', (route) => route.isFirst);
+            break;
+          case 3:
+            Navigator.of(context)
+                .pushNamedAndRemoveUntil('/profile', (route) => route.isFirst);
+
+            break;
+        }
+      },
+      // surfaceTintColor: const Color.fromARGB(255, 236, 101, 101),
+      destinations: const [
+        NavigationDestination(
+          selectedIcon: Icon(Icons.feed_rounded),
+          icon: Icon(Icons.feed_outlined),
+          label: 'feed',
         ),
-        GestureDetector(
-          onTapUp: (details) {
-            Navigator.of(context).pushNamedAndRemoveUntil(
-              '/map',
-              (route) => route.isFirst,
-            );
-          },
-          child: Icon(
-            Icons.map_outlined,
-            size: 32,
-            color: ModalRoute.of(context)?.settings.name == "/map"
-                ? Colors.grey
-                : Colors.black,
-          ),
+        NavigationDestination(
+          icon: Icon(Icons.map_outlined),
+          selectedIcon: Icon(Icons.map),
+          label: 'map',
         ),
-        GestureDetector(
-          onTapUp: (details) {
-            Navigator.of(context).pushNamedAndRemoveUntil(
-              '/chat',
-              (route) => route.isFirst,
-            );
-          },
-          child: Icon(
-            Icons.message_outlined,
-            size: 32,
-            color: ModalRoute.of(context)?.settings.name == "/chat"
-                ? Colors.grey
-                : Colors.black,
-          ),
+        NavigationDestination(
+          icon: Icon(Icons.message_outlined),
+          selectedIcon: Icon(Icons.message_rounded),
+          label: 'chat',
         ),
-        GestureDetector(
-          onTapUp: (details) {
-            Navigator.of(context).pushNamedAndRemoveUntil(
-              '/profile',
-              (route) => route.isFirst,
-            );
-          },
-          child: Icon(
-            Icons.person_2_outlined,
-            size: 32,
-            color: ModalRoute.of(context)?.settings.name == "/profile"
-                ? Colors.grey
-                : Colors.black,
-          ),
+        NavigationDestination(
+          icon: Icon(Icons.person_4_outlined),
+          selectedIcon: Icon(Icons.person_4_rounded),
+          label: 'profile',
         ),
       ],
     );
