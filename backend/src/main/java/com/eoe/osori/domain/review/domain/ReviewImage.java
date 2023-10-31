4db4beffdb5f280413d12f47b1907cec5e127b84
정의석 <euiseok96@gmail.com>
2023-10-31 17:28:58 +0900

    :sparkles: ReviewImage.java 리뷰 이미지 엔티티 추가

---
+++ backend/src/main/java/com/eoe/osori/domain/review/domain/ReviewImage.java
@@ -0,0 +1,33 @@
+package com.eoe.osori.domain.review.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "review_image")
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ReviewImage {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Size(max = 255)
+	@Column
+	private String url;
+
+	@NotNull
+	@Column
+	private Long reviewId;
+}
