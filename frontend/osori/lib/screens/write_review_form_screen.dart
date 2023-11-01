6b839f044db1baf227db36d0661de562682d1b97
sweetandsourkiss <mooul0529@gmail.com>
2023-11-01 16:28:52 +0900

    :sparkles: 리뷰 폼 스크린
    영수증 스캔으로 얻는 정보
    - 가게명
    - 방문 날짜
    - 총 금액
    유저 기입 정보
    - 방문 인원
    - 평점
    - 결제 유형
    - 단위(factor)
    - 사진
    - 내용

--- frontend/osori/lib/screens/write_review_form_screen.dart
+++ frontend/osori/lib/screens/write_review_form_screen.dart
@@ -1,17 +1,223 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-class WriteReviewFormScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:image_picker/image_picker.dart';
+
+class WriteReviewFormScreen extends StatefulWidget {
   const WriteReviewFormScreen({super.key});
 
   @override
+  State<WriteReviewFormScreen> createState() => _WriteReviewFormScreenState();
+}
+
+class _WriteReviewFormScreenState extends State<WriteReviewFormScreen> {
+  String typeUnit = "";
+  final List<File> _selectedImages = [];
+  @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    List<String> billTypeOptions = ["횟수", "시간", "일일", "개월", "금액", "기타"];
+    List<double> rates = [0, 0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5];
     return Scaffold(
       appBar: AppBar(
-        title: const Text('세부 정보'),
+        title: const Text('상세 정보'),
         centerTitle: true,
       ),
-      body: const Center(
-        child: Text("Hi Write Form"),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+              horizontal: size.width / 10, vertical: size.height / 30),
+          child: FormBuilder(
+              child: Column(
+            children: [
+              FormBuilderTextField(
+                name: "date",
+                initialValue: "2023.11.01",
+                decoration: const InputDecoration(
+                  labelText: "방문 날짜",
+                  prefixIcon: Icon(Icons.date_range),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(20),
+                    ),
+                  ),
+                ),
+                enabled: false,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              FormBuilderTextField(
+                name: "price",
+                initialValue: "000,000",
+                decoration: const InputDecoration(
+                  labelText: "총 금액",
+                  suffixText: "원",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(20),
+                    ),
+                  ),
+                ),
+                enabled: false,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  SizedBox(
+                    width: size.width / 3,
+                    child: FormBuilderTextField(
+                      name: "headCount",
+                      keyboardType: TextInputType.number,
+                      decoration: const InputDecoration(
+                        labelText: "방문 인원",
+                        suffixText: "명",
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(20),
+                          ),
+                        ),
+                      ),
+                      // validator: FormBuilderValidators.compose([
+                      //   FormBuilderValidators.required(),
+                      // ]),
+                    ),
+                  ),
+                  SizedBox(
+                    width: size.width / 3,
+                    child: FormBuilderDropdown<double>(
+                      name: 'rate',
+                      decoration: const InputDecoration(
+                        labelText: '평점',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(20),
+                          ),
+                        ),
+                      ),
+                      items: rates
+                          .map((rate) => DropdownMenuItem(
+                                value: rate,
+                                alignment: Alignment.center,
+                                child: Text("$rate"),
+                              ))
+                          .toList(),
+                    ),
+                  )
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  SizedBox(
+                    width: size.width / 3,
+                    child: FormBuilderDropdown<String>(
+                      name: "billType",
+                      initialValue: typeUnit,
+                      onChanged: (context) {
+                        setState(() {
+                          typeUnit = context!;
+                        });
+                      },
+                      decoration: const InputDecoration(
+                        labelText: "결제 유형",
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(20),
+                          ),
+                        ),
+                      ),
+                      items: billTypeOptions
+                          .map(
+                            (option) => DropdownMenuItem(
+                              alignment: AlignmentDirectional.center,
+                              value: option,
+                              child: Text(option),
+                            ),
+                          )
+                          .toList(),
+                    ),
+                  ),
+                  SizedBox(
+                    width: size.width / 3,
+                    child: FormBuilderTextField(
+                      name: "factor",
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(
+                        labelText: '단위',
+                        suffixText: typeUnit,
+                        border: const OutlineInputBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(20),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              if (_selectedImages.isNotEmpty)
+                SingleChildScrollView(
+                  clipBehavior: Clip.hardEdge,
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: [
+                      for (var image in _selectedImages)
+                        Image.file(
+                          image,
+                          fit: BoxFit.cover,
+                          width: size.width / 4,
+                          height: size.width / 4,
+                        ),
+                    ],
+                  ),
+                ),
+              IconButton(
+                icon: const Icon(Icons.add_photo_alternate_outlined),
+                onPressed: () async {
+                  final imagePicker = ImagePicker();
+                  final pickedImages = await imagePicker.pickMultiImage();
+                  // print(pickedImages);
+                  for (var image in pickedImages) {
+                    _selectedImages.add(File(image.path));
+                  }
+                  setState(() {});
+                },
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              FormBuilderTextField(
+                maxLines: 2,
+                minLines: 1,
+                name: 'content',
+                decoration: const InputDecoration(
+                  labelText: '리뷰',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(20),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(onPressed: () {}, child: const Text("리뷰 등록하기")),
+            ],
+          )),
+        ),
       ),
     );
   }
