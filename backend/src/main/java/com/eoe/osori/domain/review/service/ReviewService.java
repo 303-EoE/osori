064e1139cd58df2e8b641f69748fdf9100243dbd
정의석 <euiseok96@gmail.com>
2023-11-01 13:51:36 +0900

    :sparkles: ReviewService.java ReviewServiceImpl.java 리뷰 등록 로직 수정 MultipartFile을 리뷰 등록 시 받아오도록 수정

--- backend/src/main/java/com/eoe/osori/domain/review/service/ReviewService.java
+++ backend/src/main/java/com/eoe/osori/domain/review/service/ReviewService.java
@@ -1,12 +1,16 @@
 package com.eoe.osori.domain.review.service;
 
+import java.util.List;
+
+import org.springframework.web.multipart.MultipartFile;
+
 import com.eoe.osori.domain.review.dto.GetReviewDetailResponseDto;
 import com.eoe.osori.domain.review.dto.PostReviewRequestDto;
 import com.eoe.osori.global.common.response.CommonIdResponseDto;
 
 public interface ReviewService {
 
-	CommonIdResponseDto saveReview(PostReviewRequestDto postReviewRequestDto);
+	CommonIdResponseDto saveReview(PostReviewRequestDto postReviewRequestDto, List<MultipartFile> reviewImages);
 	
 	// 후에 멤버 검증 로직 추가
 	void deleteReview(Long reviewId, Long memberId);
--- backend/src/main/java/com/eoe/osori/domain/review/service/ReviewServiceImpl.java
+++ backend/src/main/java/com/eoe/osori/domain/review/service/ReviewServiceImpl.java
@@ -1,13 +1,19 @@
 package com.eoe.osori.domain.review.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.eoe.osori.domain.review.domain.Review;
+import com.eoe.osori.domain.review.domain.ReviewImage;
 import com.eoe.osori.domain.review.dto.GetMemberResponseDto;
 import com.eoe.osori.domain.review.dto.GetReviewDetailResponseDto;
 import com.eoe.osori.domain.review.dto.GetStoreResponseDto;
 import com.eoe.osori.domain.review.dto.PostReviewRequestDto;
+import com.eoe.osori.domain.review.repository.ReviewImageRepository;
 import com.eoe.osori.domain.review.repository.ReviewRepository;
 import com.eoe.osori.global.advice.error.exception.ReviewException;
 import com.eoe.osori.global.advice.error.info.ReviewErrorInfo;
@@ -22,18 +28,20 @@ import lombok.extern.slf4j.Slf4j;
 public class ReviewServiceImpl implements ReviewService {
 
 	private final ReviewRepository reviewRepository;
+	private final ReviewImageRepository reviewImageRepository;
 
 	/**
 	 *
 	 * 리뷰 등록
 	 *
 	 * @param postReviewRequestDto PostReviewRequestDto
+	 * @param reviewImages List<MultipartFile>
 	 * @return CommonIdResponseDto
 	 * @see ReviewRepository
 	 */
 	@Transactional
 	@Override
-	public CommonIdResponseDto saveReview(PostReviewRequestDto postReviewRequestDto) {
+	public CommonIdResponseDto saveReview(PostReviewRequestDto postReviewRequestDto, List<MultipartFile> reviewImages) {
 		if (postReviewRequestDto.findEmptyValue()) {
 			throw new ReviewException(ReviewErrorInfo.INVALID_REVIEW_REQUEST_DATA_ERROR);
 		}
@@ -47,7 +55,6 @@ public class ReviewServiceImpl implements ReviewService {
 		Review review = Review.from(postReviewRequestDto);
 
 		review.updateAverageCost(review.getTotalPrice(), review.getFactor(), review.getHeadcount());
-
 		/*
 		지울거
 		 */
@@ -57,6 +64,28 @@ public class ReviewServiceImpl implements ReviewService {
 		 */
 
 		reviewRepository.save(review);
+
+
+		// 일단 프론트에서 List<MultipartFile> 받은 거 고대로 api 통신해서 image 처리하는 데로 보내주고
+		// 그걸 다시 api 통신으로 List<String> reviewImageList 받고 (이미지 url 리스트들만 들어옴)
+		// 이걸 reviewImage 엔티티랑 연결해서 저장
+		// 이걸 통신으로 보내고
+		// List<MultipartFile> reviewImageList = reviewImages;
+
+		// 통신 구현해서 받자
+		List<String> reviewImageUrlList = new ArrayList<>();
+
+		reviewImageUrlList.add("https://avatars.githubusercontent.com/u/118112177?v=4");
+		reviewImageUrlList.add("https://avatars.githubusercontent.com/u/122416904?v=4");
+
+		for (int i = 0; i < reviewImageUrlList.size(); i++) {
+			String imageUrl = reviewImageUrlList.get(i);
+
+			ReviewImage reviewImage = ReviewImage.of(review.getId(), imageUrl);
+
+			reviewImageRepository.save(reviewImage);
+		}
+
 
 		return CommonIdResponseDto.from(review.getId());
 	}
