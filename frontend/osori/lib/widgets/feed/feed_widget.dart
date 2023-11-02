abc26b783dcc43cfc34b74020f914c5609701afa
sweetandsourkiss <mooul0529@gmail.com>
2023-11-02 15:08:06 +0900

    :art: 피드 스크린 위젯 구조 변경

--- frontend/osori/lib/widgets/feed/feed_widget.dart
+++ frontend/osori/lib/widgets/feed/feed_widget.dart
@@ -1,144 +1,143 @@
 import 'package:flutter/material.dart';
 
 class Feed extends StatelessWidget {
+  final int idx;
   const Feed({
     super.key,
+    required this.idx,
   });
 
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return Column(
-      children: [
-        for (var i in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-          Container(
-            // height: 600,
-            // margin: const EdgeInsets.only(bottom: 10),
-            decoration: const BoxDecoration(
-              color: Color(0xFFf9f8f8),
-              border: Border.symmetric(
-                horizontal: BorderSide(width: 0.1),
-              ),
+    return Container(
+      // height: 600,
+      // margin: const EdgeInsets.only(bottom: 10),
+      decoration: const BoxDecoration(
+        color: Color(0xFFf9f8f8),
+        border: Border.symmetric(
+          horizontal: BorderSide(width: 0.1),
+        ),
+      ),
+      child: Column(children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+          child:
+              Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+            Row(
+              children: [
+                Image.asset(
+                  'assets/images/288X288.png',
+                  height: 32,
+                ),
+                const SizedBox(
+                  width: 16,
+                ),
+                SizedBox(
+                  width: size.width / 2,
+                  child: const Text(
+                    'nicknamename',
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+              ],
             ),
-            child: Column(children: [
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-                child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Row(
-                        children: [
-                          Image.asset(
-                            'assets/images/288X288.png',
-                            height: 32,
-                          ),
-                          const SizedBox(
-                            width: 16,
-                          ),
-                          SizedBox(
-                            width: size.width / 2,
-                            child: const Text(
-                              'nicknamename',
-                              overflow: TextOverflow.ellipsis,
-                            ),
-                          ),
-                        ],
-                      ),
-                      const Icon(Icons.more_vert_outlined),
-                    ]),
-              ),
-              Image.asset(
-                i % 2 == 1
-                    ? 'assets/images/test.jpg'
-                    : 'assets/images/testWidth.jpg',
-                width: size.width,
-                height: size.width,
-                fit: BoxFit.cover,
-              ),
-              const Padding(
-                padding: EdgeInsets.all(10),
-                child: Column(children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Flexible(
-                        child: Text(
-                          "어떤 가게",
-                          overflow: TextOverflow.ellipsis,
-                          style: TextStyle(
-                              fontSize: 24, fontWeight: FontWeight.w600),
-                        ),
-                      ),
-                      Icon(
-                        Icons.favorite_outline,
-                        size: 32,
-                      )
-                    ],
+            const Icon(Icons.more_vert_outlined),
+          ]),
+        ),
+        AspectRatio(
+          aspectRatio: 1,
+          child: Image.asset(
+            idx % 2 == 1
+                ? 'assets/images/test.jpg'
+                : 'assets/images/testWidth.jpg',
+            width: size.width,
+            fit: BoxFit.cover,
+          ),
+        ),
+        const Padding(
+          padding: EdgeInsets.all(10),
+          child: Column(children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Flexible(
+                  child: Text(
+                    "어떤 가게",
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.w600),
                   ),
-                  SizedBox(
-                    height: 10,
+                ),
+                Icon(
+                  Icons.favorite_outline,
+                  size: 32,
+                )
+              ],
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Row(
+              children: [
+                Text(
+                  "000,000원",
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w500,
                   ),
-                  Row(
-                    children: [
-                      Text(
-                        "000,000원",
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ),
-                      SizedBox(
-                        width: 20,
-                      ),
-                      Row(
-                        children: [
-                          Icon(
-                            Icons.star_rounded,
-                            color: Color.fromARGB(255, 255, 230, 0),
-                          ),
-                          Icon(
-                            Icons.star_rounded,
-                            color: Colors.yellow,
-                          ),
-                          Icon(
-                            Icons.star_rounded,
-                            color: Colors.yellow,
-                          ),
-                          Icon(
-                            Icons.star_half_rounded,
-                            color: Colors.yellow,
-                          ),
-                          Icon(
-                            Icons.star_outline_rounded,
-                            color: Colors.yellow,
-                          ),
-                        ],
-                      ),
-                      Text("3.5"),
-                    ],
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Text(
-                      "짱긴 텍스트지롱 헤헤 짱긴 텍스트지롱 헤헤 짱긴 텍스트지롱 헤헤 짱긴 텍스트지롱 헤헤 짱긴 텍스트지롱 헤헤 짱긴 텍스트지롱 헤헤 짱긴 텍스트지롱 헤헤 짱긴 텍스트지롱 헤헤 짱긴 텍스트지롱 헤헤 짱긴 텍스트지롱 헤헤 짱긴 텍스트지롱 헤헤 짱긴 텍스트지롱 헤헤 짱긴 텍스트지롱 헤헤 짱긴 텍스트지롱 헤헤 짱긴 텍스트지롱 헤헤 짱긴 텍스트지롱 헤헤 "),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Row(
-                    children: [
-                      Text(
-                        '장소 | 20xx년 yy월 dd일',
-                        style: TextStyle(color: Colors.grey),
-                      ),
-                    ],
-                  ),
-                ]),
-              )
-            ]),
-          )
-      ],
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                Row(
+                  children: [
+                    Icon(
+                      Icons.star_rounded,
+                      color: Color.fromARGB(255, 255, 230, 0),
+                    ),
+                    Icon(
+                      Icons.star_rounded,
+                      color: Colors.yellow,
+                    ),
+                    Icon(
+                      Icons.star_rounded,
+                      color: Colors.yellow,
+                    ),
+                    Icon(
+                      Icons.star_half_rounded,
+                      color: Colors.yellow,
+                    ),
+                    Icon(
+                      Icons.star_outline_rounded,
+                      color: Colors.yellow,
+                    ),
+                  ],
+                ),
+                Text("3.5"),
+              ],
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              "짱긴 텍스트지롱 헤헤 짱긴 텍스트지롱 헤헤 짱긴 텍스트지롱 헤헤 짱긴 텍스트지롱 헤헤 짱긴 텍스트지롱 헤헤 짱긴 텍스트지롱 헤헤 짱긴 텍스트지롱 헤헤 짱긴 텍스트지롱 헤헤 짱긴 텍스트지롱 헤헤 짱긴 텍스트지롱 헤헤 짱긴 텍스트지롱 헤헤 짱긴 텍스트지롱 헤헤 짱긴 텍스트지롱 헤헤 짱긴 텍스트지롱 헤헤 짱긴 텍스트지롱 헤헤 짱긴 텍스트지롱 헤헤 ",
+              maxLines: 5,
+              overflow: TextOverflow.ellipsis,
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Row(
+              children: [
+                Text(
+                  '장소 | 20xx년 yy월 dd일',
+                  style: TextStyle(color: Colors.grey),
+                ),
+              ],
+            ),
+          ]),
+        )
+      ]),
     );
   }
 }
