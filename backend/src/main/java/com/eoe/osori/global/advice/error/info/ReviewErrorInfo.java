490bd05d2f1f3e817b6b3eadabbd72ca21653b66
정의석 <euiseok96@gmail.com>
2023-11-03 18:14:40 +0900

    :bug: ReviewController.java ReviewErrorInfo.java ReviewServiceImpl.java 리뷰 등록 버그 수정 리뷰 등록 시 Multipartfile을 Null로 인식하는 버그 해결

--- backend/src/main/java/com/eoe/osori/global/advice/error/info/ReviewErrorInfo.java
+++ backend/src/main/java/com/eoe/osori/global/advice/error/info/ReviewErrorInfo.java
@@ -9,7 +9,8 @@ public enum ReviewErrorInfo {
 	INVALID_REVIEW_REQUEST_DATA_ERROR(HttpStatus.BAD_REQUEST, 1200, "유효하지 않은 요청 정보입니다."),
 	DUPLICATE_RECEIPT_REQUEST_ERROR(HttpStatus.BAD_REQUEST, 1201, "이미 리뷰가 등록된 영수증입니다."),
 	NOT_FOUND_REVIEW_BY_ID(HttpStatus.BAD_REQUEST, 1202, "해당 리뷰가 존재하지 않습니다."),
-	NOT_MATCH_REVIEW_BY_MEMBERID(HttpStatus.BAD_REQUEST, 1203, "본인이 작성한 리뷰가 아닙니다.");
+	NOT_MATCH_REVIEW_BY_MEMBERID(HttpStatus.BAD_REQUEST, 1203, "본인이 작성한 리뷰가 아닙니다."),
+	INVALID_REVIEW_IMAGES_DATA_ERROR(HttpStatus.BAD_REQUEST, 1204, "리뷰 이미지가 등록되지 않았습니다.");
 
 	private final HttpStatus status;
 	private final Integer code;
