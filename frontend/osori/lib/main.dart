63808b40aca68a7202d3bde230d6f63b060f011c
sask <mooul0529@gmail.com>
2023-11-05 23:36:20 +0900

    :memo: .env 키 이름 수정

--- frontend/osori/lib/main.dart
+++ frontend/osori/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 import 'package:kakao_map_plugin/kakao_map_plugin.dart';
 import 'package:osori/screens/chat_screen.dart';
 import 'package:osori/screens/feed_screen.dart';
@@ -9,8 +10,14 @@ import 'package:osori/screens/profile_screen.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  // runApp() 호출 전 Flutter SDK 초기화
+
   await dotenv.load(fileName: 'assets/env/.env');
-  AuthRepository.initialize(appKey: dotenv.env['KAKAO_MAP_API_KEY'] ?? '');
+  AuthRepository.initialize(appKey: dotenv.env['KAKAO_REST_API_KEY'] ?? '');
+  KakaoSdk.init(
+    nativeAppKey: dotenv.env['KAKAO_NATIVE_APP_KEY'],
+    javaScriptAppKey: dotenv.env['KAKAO_JAVASCRIPT_APP_KEY'],
+  );
 
   runApp(const Osori());
 }
