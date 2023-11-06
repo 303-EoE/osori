82ca06592041f0d93092a4b9091f8fd684be33dd
jihye byun <jhye.byun@gmail.com>
2023-11-06 14:51:49 +0900

    :sparkles: 가게 상세 정보 조회 기능 구현

--- backend/src/main/java/com/eoe/osori/global/advice/error/info/StoreErrorInfo.java
+++ backend/src/main/java/com/eoe/osori/global/advice/error/info/StoreErrorInfo.java
@@ -9,7 +9,8 @@ public enum StoreErrorInfo {
 	INVALID_STORE_REQUEST_DATA_ERROR(HttpStatus.BAD_REQUEST, 1300, "유효하지 않은 요청 정보입니다."),
 	CANNOT_CONNECT_KAKAO_LOCAL_API(HttpStatus.INTERNAL_SERVER_ERROR, 1301, "카카오 로컬API와의 통신에 실패했습니다."),
 	ALREADY_REGISTERED_KAKAO_ID(HttpStatus.BAD_REQUEST, 1302, "이미 등록된 카카오 아이디입니다."),
-	CANNOT_FIND_STORE_BY_KAKAO_ID(HttpStatus.BAD_REQUEST, 1303, "입력된 카카오 아이디로 검색된 가게가 없습니다.");
+	CANNOT_FIND_STORE_BY_KAKAO_ID(HttpStatus.BAD_REQUEST, 1303, "입력된 카카오 아이디로 검색된 가게가 없습니다."),
+	CANNOT_FIND_STORE_BY_ID(HttpStatus.BAD_REQUEST, 1304, "입력된 아이디로 검색된 가게가 없습니다.");
 
 	private final HttpStatus status;
 	private final Integer code;
