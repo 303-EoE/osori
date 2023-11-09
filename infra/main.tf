04edbba68250eb815382d8d331f10624b4a73ca1
mydowson <mydowson@gmail.com>
2023-11-09 13:57:26 +0900

    :memo: fix main.tf
    main.tf 오타 fix

--- infra/main.tf
+++ infra/main.tf
@@ -13,7 +13,7 @@ resource "aws_s3_bucket" "osori-bucket" {
 
 resource "aws_s3_bucket_public_access_block" "public-access"{
 
-	bucket = aws_s3_bucket.osori-bucket-terraform.id
+	bucket = aws_s3_bucket.osori-bucket.id
 
 	block_public_acls	= false
 	block_public_policy	= false
