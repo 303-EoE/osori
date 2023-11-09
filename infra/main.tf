8ed8b17b5b8997457fd2ae03b6a769b6ab2509d8
mydowson <mydowson@gmail.com>
2023-11-09 13:53:05 +0900

    :sparkles: Add AWS s3 terraform file
    s3 terraform file 추가

---
+++ infra/main.tf
@@ -0,0 +1,23 @@
+
+provider "aws" {
+
+#	profile = ""
+	region = "ap-northeast-2"
+}
+
+resource "aws_s3_bucket" "osori-bucket" {
+	bucket = "osori-bucket"
+
+
+}
+
+resource "aws_s3_bucket_public_access_block" "public-access"{
+
+	bucket = aws_s3_bucket.osori-bucket-terraform.id
+
+	block_public_acls	= false
+	block_public_policy	= false
+	ignore_public_acls	= false
+	restrict_public_buckets	= false
+}
+
