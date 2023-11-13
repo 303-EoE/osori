0ef9456522ab6ef35146d623e16a4a4f16d363c5
김은솔 <kimeunsol95@naver.com>
2023-11-13 16:41:42 +0900

    :construction: 내 정보 가져오기 작성 중 내 정보 조회 작성중

--- backend/src/main/java/com/eoe/osori/domain/member/service/MemberServiceImpl.java
+++ backend/src/main/java/com/eoe/osori/domain/member/service/MemberServiceImpl.java
@@ -1,5 +1,6 @@
 package com.eoe.osori.domain.member.service;
 
+import org.apache.tomcat.util.modeler.FeatureInfo;
 import org.springframework.stereotype.Service;
 
 import com.eoe.osori.domain.member.dto.GetMemberMyPageResponseDto;
@@ -7,7 +8,10 @@ import com.eoe.osori.domain.member.repository.MemberRepository;
 import com.eoe.osori.global.advice.error.exception.MemberException;
 import com.eoe.osori.global.advice.error.info.MemberErrorInfo;
 import com.eoe.osori.global.common.api.auth.AuthApi;
+import com.eoe.osori.global.common.api.auth.dto.PostAuthInfoResponseDto;
+import com.eoe.osori.global.common.response.EnvelopeResponse;
 
+import feign.FeignException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -20,9 +24,19 @@ public class MemberServiceImpl implements MemberService{
 	private final AuthApi authApi;
 	@Override
 	public GetMemberMyPageResponseDto getMyInfo(String accessToken) {
+		log.info("내 정보 조회 : {}", accessToken);
 		// 입력값 검증
+		EnvelopeResponse<PostAuthInfoResponseDto> postAuthInfoResponseDtoEnvelopeResponse;
 
+		try{
+			postAuthInfoResponseDtoEnvelopeResponse = authApi.getMyInfo(accessToken);
+		} catch (FeignException e){
+			System.out.println(e.getMessage());
+			throw new MemberException(MemberErrorInfo.FAIL_TO_AUTH_FEIGN_CLIENT_REQUEST);
+		}
 
-		return null;
+		PostAuthInfoResponseDto postAuthInfoResponseDto = postAuthInfoResponseDtoEnvelopeResponse.getData();
+		return GetMemberMyPageResponseDto.of(postAuthInfoResponseDto.getId(), postAuthInfoResponseDto.getNickname(),
+			postAuthInfoResponseDto.getProfileImageUrl());
 	}
 }
--- backend/src/main/java/com/eoe/osori/global/advice/error/info/MemberErrorInfo.java
+++ backend/src/main/java/com/eoe/osori/global/advice/error/info/MemberErrorInfo.java
@@ -6,7 +6,8 @@ import lombok.Getter;
 
 @Getter
 public enum MemberErrorInfo {
-	INVALID_MEMBER_REQUEST_DATA_ERROR(HttpStatus.BAD_REQUEST, 1100, "유효하지 않은 요청 정보입니다.");
+	INVALID_MEMBER_REQUEST_DATA_ERROR(HttpStatus.BAD_REQUEST, 1100, "유효하지 않은 요청 정보입니다."),
+	FAIL_TO_AUTH_FEIGN_CLIENT_REQUEST(HttpStatus.BAD_REQUEST, 1101, "Auth Feign Client 통신에 실패했습니다.");
 
 	private final HttpStatus status;
 	private final Integer code;
