c03860ce15cf5008c5de1a949ec4ad5c996b625b
김은솔 <kimeunsol95@naver.com>
2023-11-14 17:27:31 +0900

    :bulb: 주석 제거 코드 주석 제거

--- backend/src/main/java/com/eoe/osori/domain/auth/service/AuthServiceImpl.java
+++ backend/src/main/java/com/eoe/osori/domain/auth/service/AuthServiceImpl.java
@@ -14,7 +14,6 @@ import com.eoe.osori.domain.auth.dto.PostAuthLoginRequestDto;
 import com.eoe.osori.domain.auth.dto.PostAuthLoginResponseDto;
 import com.eoe.osori.domain.auth.dto.PostAuthProfileRequestDto;
 import com.eoe.osori.domain.auth.dto.PostAuthProfileResponseDto;
-import com.eoe.osori.domain.auth.dto.PostAuthReissueTokenResponseDto;
 import com.eoe.osori.domain.auth.repository.MemberRepository;
 import com.eoe.osori.global.advice.error.exception.AuthException;
 import com.eoe.osori.global.advice.error.info.AuthErrorInfo;
