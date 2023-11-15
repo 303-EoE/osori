31dda03dccce4991caff45709eb1bc646047f210
mydowson <mydowson@gmail.com>
2023-11-15 14:35:18 +0900

    :construction_worker: Add Terraform files
    ec2, s3, security_groups terraform 파일 추가

---
+++ infra/s3.tf
@@ -0,0 +1,18 @@
+#S3
+
+resource "aws_s3_bucket" "osori-bucket" {
+        bucket = "osori-bucket"
+
+
+}
+
+resource "aws_s3_bucket_public_access_block" "public-access"{
+
+        bucket = aws_s3_bucket.osori-bucket.id
+
+        block_public_acls       = false
+        block_public_policy     = false
+        ignore_public_acls      = false
+        restrict_public_buckets = false
+}
+
