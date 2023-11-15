7d7e9c720cd9f0a2d685d46cc3feedd0119ce761
jihye byun <jhye.byun@gmail.com>
2023-11-15 13:59:52 +0900

    :wrench: redis cache 설정

---
+++ backend/src/main/java/com/eoe/osori/global/common/redis/config/RedisConfig.java
@@ -0,0 +1,76 @@
+package com.eoe.osori.global.common.redis.config;
+
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.CacheKeyPrefix;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+@EnableCaching
+@EnableRedisRepositories
+public class RedisConfig {
+
+	@Value("${spring.data.redis.host}")
+	private String host;
+
+	@Value("${spring.data.redis.port}")
+	private Integer port;
+
+	@Value("${spring.data.redis.password}")
+	private String password;
+
+	@Bean
+	public RedisConnectionFactory redisConnectionFactory() {
+		LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory(host, port);
+		lettuceConnectionFactory.setPassword(password);
+		return lettuceConnectionFactory;
+	}
+
+	@Bean
+	public RedisTemplate<?, ?> redisTemplate() {
+		RedisTemplate<byte[], byte[]> redisTemplate = new RedisTemplate<>();
+		redisTemplate.setConnectionFactory(redisConnectionFactory());
+		return redisTemplate;
+	}
+
+	@Bean
+	public CacheManager redisCacheManager(RedisConnectionFactory redisConnectionFactory) {
+		RedisCacheConfiguration configuration = RedisCacheConfiguration.defaultCacheConfig()
+			.disableCachingNullValues()
+			.entryTtl(Duration.ofSeconds(CacheKey.DEFAULT_EXPIRE_SEC))
+			.computePrefixWith(CacheKeyPrefix.simple())
+			.serializeKeysWith(
+				RedisSerializationContext.SerializationPair
+					.fromSerializer(new StringRedisSerializer()))
+
+			.serializeValuesWith(RedisSerializationContext
+				.SerializationPair
+				.fromSerializer(new GenericJackson2JsonRedisSerializer()));
+
+		// 캐시키 별 default 유효시간 설정
+		Map<String, RedisCacheConfiguration> cacheConfiguration = new HashMap<>();
+		cacheConfiguration.put(CacheKey.ZONE, RedisCacheConfiguration.defaultCacheConfig()
+			.entryTtl(Duration.ofSeconds(CacheKey.ZONE_EXPIRE_SEC)));
+
+		return RedisCacheManager.RedisCacheManagerBuilder
+			.fromConnectionFactory(redisConnectionFactory)
+			.cacheDefaults(configuration)
+			.withInitialCacheConfigurations(cacheConfiguration)
+			.build();
+	}
+}
