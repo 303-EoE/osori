42ec6b04cc2dede506b711f36d6509574abdb296
김현수 <gustn2615@naver.com>
2023-11-16 22:41:10 +0900

    fix

--- backend/src/main/java/com/eoe/osori/A303Application.java
+++ backend/src/main/java/com/eoe/osori/A303Application.java
@@ -5,10 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@SpringBootApplication
 @EnableFeignClients
 public class A303Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(A303Application.class, args);
 	}
